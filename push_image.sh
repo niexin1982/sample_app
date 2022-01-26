f53f4a515187464483869aec4e2a34843de3d4de
bun913 <imanau03@gmail.com>
2022-01-26 18:15:25 +0900

    feat: taskdef.jsonの追加と不要ファイルの削除

--- push_image.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# login
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REPO_URI
-# build
-docker build --platform linux/amd64 -t $CONTAINER_NAME:$TAG .
-# tag
-docker tag $CONTAINER_NAME:$TAG $REPO_URI:$TAG
-# push
-docker push $REPO_URI:$TAG
