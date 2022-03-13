76ebdbd50d92f9b640b050bedcf7efac45d72df1
bun913 <imanau03@gmail.com>
2022-03-13 10:46:49 +0900

    fix: ECRへイメージをプッシュするためのスクリプトを修正

--- push_image.sh
+++ push_image.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-# login
-aws ecr get-login-password --region $AWS_REGION | docker login --username AWS --password-stdin $REPO_URI
-# build
-docker build --platform linux/amd64 -t $CONTAINER_NAME:$TAG .
-# tag
-docker tag $CONTAINER_NAME:$TAG $REPO_URI:$TAG
-# push
-docker push $REPO_URI:$TAG
+PROJECT=$1
+if [ -z "${PROJECT}" ]; then
+  echo "第1引数にプロジェクト名を指定してください"
+  exit 1
+fi
+AWS_ACCOUNT_ID=$(aws sts get-caller-identity --query "Account" --out text)
+INIT_TAG=v1
+ECR_BASE_URI=${AWS_ACCOUNT_ID}.dkr.ecr.ap-northeast-1.amazonaws.com
+ECR_PREFIX=${ECR_BASE_URI}/${PROJECT}
+# ログイン
+aws ecr get-login-password --region ap-northeast-1 | \
+ docker login --username AWS --password-stdin ${ECR_BASE_URI}
+# build app
+APP_IMAGE_FULL_NAME="${ECR_PREFIX}-app"
+docker build --platform linux/amd64 -t ${APP_IMAGE_FULL_NAME}:${INIT_TAG} .
+docker push ${APP_IMAGE_FULL_NAME}:${INIT_TAG}
